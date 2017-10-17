21a9f3cb85767a7d77c9a5a21f3011b47a2b82a0
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2017-10-17 02:10:21 -0200

    created the PCA class, tested with 4 small datasets

---
+++ include/PCA.hpp
@@ -0,0 +1,79 @@
+//
+// Created by dodo on 16/10/17.
+//
+
+#ifndef MACHINE_LEARNING_PCA_HPP
+#define MACHINE_LEARNING_PCA_HPP
+
+
+#include "Matrix.hpp"
+
+using namespace std;
+
+class PCA {
+
+private:
+    Matrix X, eigenvalues, eigenvectors, percentages;
+public :
+    explicit PCA(Matrix data) {
+        X = std::move(data);
+    }
+
+    void fit() {
+        X = X.standardize(); // standardize columns to have 0 mean and 1 std
+        Matrix covariances = X.cov(); // get covariance matrix of the data
+
+        // get the sum of variances, this'll be useful later
+        double sumVar = 0;
+        for (int i = 0; i < covariances.nRows(); i++) {
+            sumVar += covariances(i, i);
+        }
+
+        pair<Matrix, Matrix> eig = covariances.eigen(); // eigenvalues and eigenvectors of cov matrix
+        eigenvalues = Matrix(eig.first.nRows(), eig.first.nCols());
+        eigenvectors = Matrix(eig.second.nRows(), eig.second.nCols());
+
+        // keep the order of eigenvalues in this vector
+        vector<int> newOrder;
+        for (int i = 0; i < eigenvalues.nRows(); i++) {
+            int position = 0;
+            for (int j = 0; j < newOrder.size(); j++)
+                if (eig.first(i, 0) < eig.first(newOrder[j], 0))
+                    position++;
+            newOrder.insert(newOrder.begin() + position, i);
+        }
+
+        // order eigenvalues and eigenvectors by the value of the eigenvalues
+        for (int i = 0; i < newOrder.size(); i++) {
+            eigenvalues(i, 0) = eig.first(newOrder[i], 0);
+
+            for (int j = 0; j < eigenvectors.nRows(); j++) {
+                eigenvectors(j, i) = eig.second(j, newOrder[i]);
+            }
+        }
+
+        // calculate the percentage of variance that each eigenvalue "explains"
+        percentages = Matrix(eig.first.nRows(), eig.first.nCols());
+        for (int i = 0; i < eigenvalues.nRows(); i++) {
+            percentages(i, 0) = eigenvalues(i, 0) / sumVar;
+        }
+    }
+
+    Matrix transform() {
+        return eigenvectors.transpose() * X.transpose();
+    }
+
+    const Matrix &getEigenvalues() const {
+        return eigenvalues;
+    }
+
+    const Matrix &getEigenvectors() const {
+        return eigenvectors;
+    }
+
+    const Matrix &getPercentages() const {
+        return percentages;
+    }
+};
+
+#endif //MACHINE_LEARNING_PCA_HPP
