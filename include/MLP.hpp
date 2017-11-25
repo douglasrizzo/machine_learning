fc4d9a9c8e2ac01edb95e1c1164f13f7a0dd8bd2
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2017-11-25 12:16:58 -0200

    overall, this is the first version of fit() that actually worked
    presented low error on the XOR demo

--- include/MLP.hpp
+++ include/MLP.hpp
@@ -13,7 +13,7 @@ using namespace std;
 
 class MLP {
  private:
-  MatrixD data, classes, uniqueClasses, dataMean, dataDev;
+  MatrixD data, classes, dataMean, dataDev;
   vector<MatrixD> W;
   vector<size_t> hiddenConfig;
 
@@ -62,10 +62,67 @@ class MLP {
            vector<size_t> hiddenConfig,
            int maxIters,
            double learningRate = 0.01,
-           bool standardize = true) {
+           double errorThreshold = 0.0001,
+           ActivationFunction func = SIGMOID,
+           bool standardize = true,
+           bool verbose = true) {
+    size_t outputEncodingSize = y.unique().nRows();
+
+    // number of layers. even if there are no hidden layers,
+    // there will exist at least one layer of weights that will need to be fitted
+    size_t nLayers = hiddenConfig.size() < 1 ? 1 : hiddenConfig.size() + 1;
+    // initialize vector of weight matrices
+    vector<MatrixD> w = vector<MatrixD>(nLayers);
+
+    // initialize weights
+    for (int i = 0; i < nLayers; i++) {
+      size_t nIn, nOut;
+
+      // number of inputs (+1 accounts for the weight used in the bias)
+      nIn = (i == 0 ? X : w[i - 1]).nCols() + 1;
+
+      // number of outputs
+      nOut = i == w.size() - 1 ? outputEncodingSize : hiddenConfig[i];
+
+      w[i] = initUniform(nIn, nOut); // initialize layer with random numbers from a distribution
+    }
+
+    fit(X, y, w, maxIters, learningRate, errorThreshold, func, standardize);
+  }
+
+  void fit(MatrixD X,
+           MatrixD y,
+           vector<MatrixD> hiddenLayers,
+           int maxIters,
+           double learningRate = 0.01,
+           double errorThreshold = 0.0001,
+           ActivationFunction func = SIGMOID,
+           bool standardize = true,
+           bool verbose = true) {
     // create one-hot encoding for classes
     classes = y.oneHot();
-    uniqueClasses = y.unique().oneHot();
+    size_t outputEncodingSize = classes.nCols();
+
+    for (int i = 0; i < hiddenLayers.size(); ++i) {
+      size_t correct_nIn = (i == 0 ? X : hiddenLayers[i - 1]).nCols() + 1,
+          correct_nOut = i == hiddenLayers.size() - 1 ? outputEncodingSize : hiddenLayers[i + 1].nRows() - 1;
+      if (hiddenLayers[i].nRows() != correct_nIn) {
+        throw invalid_argument(
+            "Weight matrix " + to_string(i) + " input (" + to_string(hiddenLayers[i].nRows()) + ") should be ("
+                + to_string(correct_nIn) + ")");
+      }
+      if (hiddenLayers[i].nCols() != correct_nOut) {
+        throw invalid_argument(
+            "Weight matrix " + to_string(i) + " output (" + to_string(hiddenLayers[i].nCols()) + ") should be ("
+                + to_string(correct_nOut) + ")");
+      }
+    }
+
+    W = hiddenLayers;
+
+    // number of layers. even if there are no hidden layers,
+    // there will exist at least one layer of weights that will need to be fitted
+    size_t nLayers = hiddenLayers.size();
 
     if (standardize) {
       // if standardization takes place, mean and stddev are stored to be used in future predictions
@@ -75,63 +132,88 @@ class MLP {
     } else
       data = X;
 
-    // number of layers. even if there are no hidden layers,
-    // there will exist at least one layer of weights that will need to be fitted
-    size_t nLayers = hiddenConfig.size() < 1 ? 1 : hiddenConfig.size();
-
-    // initialize vector of weight matrices
-    W = vector<MatrixD>(nLayers);
-
-    //initialize weights
-    for (int i = 0; i < nLayers; i++) {
-      // number of inputs (+1 accounts for the weight used in the bias)
-      size_t nIn = (i == 0 ? data : W[i - 1]).nCols() + 1,
-          nOut = i == W.size() - 1 ? uniqueClasses.nRows() : hiddenConfig[i]; // number of outputs
-
-      W[i] = initNormal(nIn, nOut); // initialize layer with random numbers from normal distribution
+    function<double(double)> activationFunction, activationDerivative;
+    if (func == SIGMOID) {
+      activationFunction = sigmoid;
+      activationDerivative = sigmoidDerivative;
+    } else {
+      activationFunction = tanh;
+      activationDerivative = tanhDerivative;
     }
+
+    double previousLoss;
 
     // training iterations
     for (int iter = 0; iter < maxIters; iter++) {
+      // matrices used in forward pass
       // Z holds f(S), where f is the activation function
-      vector<MatrixD> Z = vector<MatrixD>(nLayers);
+      vector<MatrixD> Z(nLayers);
       // F are activation derivatives
-      vector<MatrixD> F = vector<MatrixD>(nLayers);
+      vector<MatrixD> F(nLayers);
 
-      MatrixD currentInput = X;
+      // matrices used on backpropagation
+      // D contains the loss signals for each layer
+      vector<MatrixD> D(nLayers);
+
+      MatrixD currentInput = data;
       //forward pass
       for (int i = 0; i < nLayers; i++) {
         // add the bias column to the input of the current layer
         currentInput.addColumn(MatrixD::ones(currentInput.nRows(), 1), 0);
 
         MatrixD S = currentInput * W[i]; // multiply input by weights
-        F[i] = S.apply(sigmoidDerivative); //
+        F[i] = S.apply(activationDerivative).transpose();
 
         //apply activation function, whose resulting matrix will be the next input
-        currentInput = Z[i] = S.apply(sigmoid);
+        currentInput = Z[i] = S.apply(activationFunction);
       }
 
       // backpropagation
-
-      // D contains the error signals for each layer
-      vector<MatrixD> D(nLayers);
-      // dW keeps the weight updates
-      vector<MatrixD> dW(nLayers);
-
-      // error signals are calculated backwards
+      // last layer error signal
       D[nLayers - 1] = (Z[nLayers - 1] - classes).transpose();
 
+      // calculate loss
+      double loss = 0;
+      for (size_t i = 0; i < data.nRows(); i++) {
+        double thisError = 0;
+        for (size_t j = 0; j < outputEncodingSize; j++) {
+          loss += pow(classes(i, j) - Z[nLayers - 1](i, j), 2);
+        }
+      }
+      loss /= (2 * data.nRows());
+
+      if (iter == 0)
+        previousLoss = loss;
+
+      if (verbose) {
+        char errorChar = loss == previousLoss ? '=' : loss > previousLoss ? '+' : '-';
+        cout << "it " << iter + 1 << ", loss: " << loss << ' ' << errorChar << endl;
+      }
+      if (loss < errorThreshold) {
+        break;
+      }
+      previousLoss = loss;
+
+      // in case there is nothing wrong with our loss, continue backpropagation
+      // loss signals for the intermediate layers
       for (int i = nLayers - 2; i >= 0; i--) {
-        D[i] = (F[i].hadamard(W[i] * D[i + 1]));
+        MatrixD W_noBias = W[i + 1].transpose();
+        W_noBias.removeColumn(0);
+        W_noBias = W_noBias.transpose();
+        // mxb  mxb            mxn       nxb
+        D[i] = F[i].hadamard(W_noBias * D[i + 1]);
       }
 
       // weight updates
-      dW[0] = -learningRate * (D[0] * data);
-      for (size_t i = 1; i < nLayers; i++) {
-        dW[i] = -learningRate * (D[i] * Z[i - 1]);
+      for (size_t i = 0; i < nLayers; i++) {
+        MatrixD input = i == 0 ? data : Z[i - 1];
+        input.addColumn(MatrixD::ones(input.nRows(), 1), 0);
+        //      bxm                       mxb         bxm
+        //               scalar       mxn     nxb
+        MatrixD dW = -learningRate * (D[i] * input).transpose();
+        // bxm
+        W[i] += dW;
       }
-
-      // TODO update the damn weights!
     }
   }
 
