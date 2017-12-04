1671fa325f51bc334282c11b8aaad2e4b4c1dcb4
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2017-12-04 11:01:33 -0200

    first shot ap dynamic programming

---
+++ include/DynamicProgramming.hpp
@@ -0,0 +1,13 @@
+//
+// Created by dodo on 04/12/17.
+//
+
+#ifndef MACHINE_LEARNING_DYNAMICPROGRAMMING_HPP
+#define MACHINE_LEARNING_DYNAMICPROGRAMMING_HPP
+
+#include "Matrix.hpp"
+class DynamicProgramming {
+private MatrixD V, R;
+};
+
+#endif //MACHINE_LEARNING_DYNAMICPROGRAMMING_HPP
