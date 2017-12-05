cc36a75ec1d491efd06a0aafa1e4e9ca31a7e68b
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2017-12-05 21:28:02 -0200

    figuring out which data structures to use with dynamic programming

--- include/DynamicProgramming.hpp
+++ include/DynamicProgramming.hpp
@@ -7,7 +7,30 @@
 
 #include "Matrix.hpp"
 class DynamicProgramming {
-private MatrixD V, R;
+ private:
+  MatrixD value, rewards, policy;
+  vector<MatrixD> transition;
+  double gamma;
+
+  enum ActionType { UP, DOWN, LEFT, RIGHT };
+  vector<ActionType> actions = {UP, DOWN, LEFT, RIGHT};
+
+ public:
+  DynamicProgramming(size_t height, size_t width, vector<pair<size_t, size_t>> goals)
+      : gamma(gamma) {
+    value = MatrixD::zeros(height, width);
+    rewards = MatrixD::fill(height, width, -1);
+
+    for (auto goal:goals)
+      rewards(goal.first, goal.second) = 0;
+
+    policy = MatrixD::ones(height * width, actions.size());
+
+    transition = vector<MatrixD>(height * width);
+
+    for (int i = 0; i < height * width; i++)
+      transition[i] = MatrixD::zeros(height * width, actions.size());
+  }
 };
 
 #endif //MACHINE_LEARNING_DYNAMICPROGRAMMING_HPP
